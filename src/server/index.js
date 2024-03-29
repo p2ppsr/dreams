3e9cd7c5215606109c43624b786b6e5bd59415af
Dan Feinstein <otherdaniel@gmail.com>
2024-03-29 11:40:06 -0700

    move res.sendfile

--- src/server/index.js
+++ src/server/index.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 })
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'))
+  res.sendFile(path.join(__dirname, '../../build', 'index.html'))
 })
 
 app.use(
