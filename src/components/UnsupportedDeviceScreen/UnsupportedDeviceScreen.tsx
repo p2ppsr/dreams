734499c9d18832e37c0245ee545697140963df8d
Dan Feinstein <otherdaniel@gmail.com>
2024-03-29 11:12:04 -0700

    added spawnnginx

--- src/components/UnsupportedDeviceScreen/UnsupportedDeviceScreen.tsx
+++ src/components/UnsupportedDeviceScreen/UnsupportedDeviceScreen.tsx
@@ -1,22 +1,12 @@
-import React from 'react';
+import React from 'react'
 
 const UnsupportedDeviceScreen = () => {
-    return (
-        <div style={styles.container}>
-            <h1>This App Only Works on Computers!</h1>
-            <p>Please visit this site on a Windows, macOS, or Linux computer to use the app.</p>
-        </div>
-    );
-};
+  return (
+    <div className="container">
+      <h1>This App Only Works on Computers!</h1>
+      <p>Please visit this site on a Windows, macOS, or Linux computer to use the app.</p>
+    </div>
+  )
+}
 
-const styles = {
-    container: {
-        textAlign: 'center',
-        padding: '50px',
-        maxWidth: '600px',
-        margin: '0 auto',
-        fontSize: '18px',
-    }
-};
-
-export default UnsupportedDeviceScreen;
+export default UnsupportedDeviceScreen
